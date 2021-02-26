e8524a4da36ee69c9d5a7e27e9b8a07449c511ad
Dezerey Escanuelas <dezerey11@gmail.com>
2021-02-26 15:33:17 -0800

    setup ajax, API, input field

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("hello");
+const $input = $('input[type="text"]');
+
+$("form").on("submit", handleGetImages);
+
+function handleGetImages(event) {
+  event.preventDefault();
+  const userInput = $input.val();
+
+  if (!userInput) {
+    return;
+  }
+
+  $.ajax({
+    url: "https://api.imgur.com/3/gallery/search/top/all/0.json?q=" + userInput,
+    headers: {
+      Authorization: "Client-ID d156c8a308b4525",
+    },
+  }).then(
+    (data) => {
+      console.log(data);
+      render(data);
+    },
+    (error) => {
+      console.log("bad request", error);
+    }
+  );
+}
+
+function render(imgData) {
+  console.log(imgData.data[0].title);
+}
