5bdc9026a3f9025e82b43130945d3f82caac1586
Dezerey Escanuelas <dezerey11@gmail.com>
2021-02-27 15:13:47 -0800

    added a match check and game ending

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ function duplicateArray(arr) {
   return arr.concat(arr);
 }
 
+// next line will only work on boxes at this time,not the changes
 $(".box").on("click", handleShowImage);
 
 function handleShowImage() {
@@ -73,5 +74,39 @@ function handleShowImage() {
   } else {
     image.show();
   }
-  console.log(this);
+  handleCheckMatch(image);
+}
+
+let firstPick = null;
+const $message = $(".message");
+
+function handleCheckMatch(image) {
+  const imgSrc = image.attr("src");
+  const imgId = image.attr("id");
+  if (!firstPick) {
+    firstPick = { src: imgSrc, id: imgId };
+    $message.text("Choose Another Card");
+  } else if (firstPick.src === imgSrc && firstPick.id !== imgId) {
+    $("#" + firstPick.id)
+      .parent()
+      .replaceWith(`<div class="box bz"></div>`);
+    image.parent().replaceWith(`<div class="box bz"></div>`);
+    firstPick = null;
+    $message.text("Match");
+    handleGameEnding();
+  } else {
+    //hide() doesn't work with a string. Selector needs # because your chosing ID.
+    $("#" + firstPick.id).hide();
+    //image is the second pick in this condition
+    image.hide();
+    firstPick = null;
+    $message.text("No Match");
+  }
+}
+
+// Check game ending ----game can only end after a match
+function handleGameEnding() {
+  if ($(".box").not(".bz").length === 0) {
+    $message.text("You did it!");
+  }
 }
